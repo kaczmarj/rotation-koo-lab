abb35ded27b2ff66dda407fff5f717f2efee8059
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2021-03-04 10:52:25 -0500

    move setup configuration to setup.cfg

--- setup.py
+++ setup.py
@@ -1,12 +1,5 @@
 from setuptools import setup
 
 setup(
-    name="chipseq_utils",
-    version="0.1.0-dev0",
-    description="Utilities for ChIP-seq analysis",
-    author="Jakub Kaczmarzyk",
-    py_modules=["chipseq_utils"],
-    python_requires=">=3.6,<4",
-    install_requires=["h5py", "matplotlib", "numpy", "pandas", "scipy"],
-    extras_require={"dev": ["flake8", "mypy"]},
-)
+        name="chipseq_utils",
+    )
